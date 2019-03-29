8778f3ea7f26da5b473c7e0782b540fd64b77619
Deniz Toprak <contact.deniz.toprak@gmail.com>
2019-03-29 19:11:59 +0100

    Entity class was not in ECS namespace causing building errors

--- UrsusEngine/UrsusEngine/Patterns/ECS/Entity.h
+++ UrsusEngine/UrsusEngine/Patterns/ECS/Entity.h
@@ -6,80 +6,84 @@
 
 namespace UrsusEngine
 {
-	class Entity
+	namespace ECS
 	{
-	public:
-
-		Entity(){}
-
-		~Entity(){}
-
-	public:
-		template<typename T>
-		std::shared_ptr<T> AddComponent()
+		class Entity
 		{
-			//check if T is child of ECS::IComponent
-			static_assert(std::is_base_of<ECS::IComponent, T>::value, "T must be derived from ECS::IComponent");
-			//Is Component already in list?
-			std::shared_ptr<T> foundComponent = GetComponent<T>();
-			if (foundComponent != nullptr)
+		public:
+
+			Entity() {}
+
+			~Entity() {}
+
+		public:
+			template<typename T>
+			std::shared_ptr<T> AddComponent()
 			{
-				return foundComponent;
+				//check if T is child of ECS::IComponent
+				static_assert(std::is_base_of<ECS::IComponent, T>::value, "T must be derived from ECS::IComponent");
+				//Is Component already in list?
+				std::shared_ptr<T> foundComponent = GetComponent<T>();
+				if (foundComponent != nullptr)
+				{
+					return foundComponent;
+				}
+
+				std::shared_ptr<T> component = std::make_shared<T>();
+				m_Components.push_back(std::shared_ptr<T>(component));
+				return component;
 			}
 
-			std::shared_ptr<T> component = std::make_shared<T>();
-			m_Components.push_back(std::shared_ptr<T>(component));
-			return component;
-		}
-
-		template<typename T>
-		void RemoveComponent()
-		{
-			//check if T is child of ECS::IComponent
-			static_assert(std::is_base_of<ECS::IComponent, T>::value, "T must be derived from ECS::IComponent");
-			//try find component and then delete
-			for (std::vector<std::shared_ptr<ECS::IComponent>>::iterator compItr = m_Components.begin(); compItr != m_Components.end();)
+			template<typename T>
+			void RemoveComponent()
 			{
-				if (std::shared_ptr<T> cast = std::dynamic_pointer_cast<T>(*compItr))
+				//check if T is child of ECS::IComponent
+				static_assert(std::is_base_of<ECS::IComponent, T>::value, "T must be derived from ECS::IComponent");
+				//try find component and then delete
+				for (std::vector<std::shared_ptr<ECS::IComponent>>::iterator compItr = m_Components.begin(); compItr != m_Components.end();)
 				{
-					m_Components.erase(compItr);
-					break;
-				}
-				else
-				{
-					++compItr;
+					if (std::shared_ptr<T> cast = std::dynamic_pointer_cast<T>(*compItr))
+					{
+						m_Components.erase(compItr);
+						break;
+					}
+					else
+					{
+						++compItr;
+					}
 				}
 			}
-		}
 
-		template<typename T>
-		std::shared_ptr<T> GetComponent()
-		{
-			//check if T is child of ECS::IComponent
-			static_assert(std::is_base_of<ECS::IComponent, T>::value, "T must be derived from ECS::IComponent");
-			//Try find component and then return
-			for (std::vector<std::shared_ptr<ECS::IComponent>>::iterator compItr = m_Components.begin(); compItr != m_Components.end();)
+			template<typename T>
+			std::shared_ptr<T> GetComponent()
 			{
-				if (std::shared_ptr<T> cast = std::dynamic_pointer_cast<T>(*compItr))
+				//check if T is child of ECS::IComponent
+				static_assert(std::is_base_of<ECS::IComponent, T>::value, "T must be derived from ECS::IComponent");
+				//Try find component and then return
+				for (std::vector<std::shared_ptr<ECS::IComponent>>::iterator compItr = m_Components.begin(); compItr != m_Components.end();)
 				{
-					return cast;
+					if (std::shared_ptr<T> cast = std::dynamic_pointer_cast<T>(*compItr))
+					{
+						return cast;
+					}
+					else
+					{
+						++compItr;
+					}
 				}
-				else
-				{
-					++compItr;
-				}
+				//Not found - return nullptr
+				return nullptr;
 			}
-			//Not found - return nullptr
-			return nullptr;
-		}
 
-		template<typename T>
-		bool HasComponent()
-		{
-			return GetComponent<T>() != nullptr;
-		}
+			template<typename T>
+			bool HasComponent()
+			{
+				return GetComponent<T>() != nullptr;
+			}
 
-	protected:
-		std::vector<std::shared_ptr<ECS::IComponent>> m_Components;
-	};
+		protected:
+			std::vector<std::shared_ptr<ECS::IComponent>> m_Components;
+		};
+
+	}
 }
