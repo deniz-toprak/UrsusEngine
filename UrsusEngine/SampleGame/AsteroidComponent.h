e24111ed031abd211b2d5c4dc7b7f114a53a85b1
Deniz Toprak <contact.deniz.toprak@gmail.com>
2019-03-31 16:43:22 +0200

    Delete old files - new game inc!

--- UrsusEngine/SampleGame/AsteroidComponent.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../UrsusEngine/Patterns/ECS/IComponent.h"
-
-class AsteroidComponent : public UrsusEngine::ECS::IComponent
-{
-public:
-	AsteroidComponent() {}
-	~AsteroidComponent(){}
-};
